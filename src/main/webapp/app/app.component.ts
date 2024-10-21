ec3be5fcf4ceb172a3c19a9ab1325b16dc50007c
Arsen A <powerm4f12@gmail.com>
2024-10-21 10:24:11 -0400

    Added services for login page.

--- src/main/webapp/app/app.component.ts
+++ src/main/webapp/app/app.component.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { RouterLink, RouterOutlet, RouterLinkActive } from '@angular/router';
 import { AdminSidebarComponent } from "./admin-sidebar/admin-sidebar.component";
 import { DashboardComponent } from "./dashboard/dashboard.component";
 import { ReservationsComponent } from './reservations/reservations.component';
+import LoginComponent from "./login/login.component";
+import { ApplicationConfigService } from 'app/core/config/application-config.service';
 
 @Component({
   selector: 'app-root',
@@ -15,11 +17,18 @@ import { ReservationsComponent } from './reservations/reservations.component';
     CommonModule,
     AdminSidebarComponent,
     ReservationsComponent,
-    DashboardComponent
+    DashboardComponent,
+    LoginComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   currentPage: string = 'dashboard';
+
+  private applicationConfigService = inject(ApplicationConfigService);
+
+  constructor() {
+    // this.applicationConfigService.setEndpointPrefix('http://localhost:8080');
+  }
 }
--- src/main/webapp/app/app.routes.ts
+++ src/main/webapp/app/app.routes.ts
@@ -1,13 +1,16 @@
-import { Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { ReservationsComponent } from './reservations/reservations.component';
-import { PricingComponent } from './pricing/pricing.component';
-import { MembersComponent } from './members/members.component';
+    import { Routes } from '@angular/router';
+    import { DashboardComponent } from './dashboard/dashboard.component';
+    import { ReservationsComponent } from './reservations/reservations.component';
+    import { PricingComponent } from './pricing/pricing.component';
+    import { MembersComponent } from './members/members.component';
+    import LoginComponent from "./login/login.component";
 
-export const routes: Routes = [
-    { path: 'dashboard', component: DashboardComponent },
-    { path: 'reservations', component: ReservationsComponent },
-    { path: 'pricing', component: PricingComponent },
-    { path: 'members', component: MembersComponent },
-    { path: '',   redirectTo: '/dashboard', pathMatch: 'full' }, 
-];
+    export const routes: Routes = [
+
+        { path: 'reservations', component: ReservationsComponent },
+        { path: 'pricing', component: PricingComponent },
+        { path: 'members', component: MembersComponent },
+        { path: 'login', component: LoginComponent },
+        { path: 'dashboard', component: DashboardComponent },
+        { path: '',   redirectTo: '/dashboard', pathMatch: 'full' },
+    ];
