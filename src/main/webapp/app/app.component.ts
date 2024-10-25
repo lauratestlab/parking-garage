770c7cd84972f9eb6e105ccd9bb40cd65e78e890
Arsen A <powerm4f12@gmail.com>
2024-10-25 18:21:04 -0400

    added build with custom webpack

--- src/main/webapp/app/app.component.ts
+++ src/main/webapp/app/app.component.ts
@@ -1,5 +1,8 @@
 import { CommonModule } from '@angular/common';
+import { registerLocaleData } from '@angular/common';
+import dayjs from 'dayjs/esm';
 import {Component, inject} from '@angular/core';
+import { NgbDatepickerConfig } from '@ng-bootstrap/ng-bootstrap';
 import { RouterLink, RouterOutlet, RouterLinkActive } from '@angular/router';
 import { AdminSidebarComponent } from "./admin-sidebar/admin-sidebar.component";
 import { DashboardComponent } from "./dashboard/dashboard.component";
@@ -7,6 +10,7 @@ import { ReservationsComponent } from './reservations/reservations.component';
 import {FormsModule} from "@angular/forms";
 import LoginComponent from "./login/login.component";
 import { ApplicationConfigService } from 'app/core/config/application-config.service';
+import locale from '@angular/common/locales/en';
 
 @Component({
   selector: 'app-root',
@@ -29,8 +33,11 @@ export default class AppComponent {
   currentPage: string = 'dashboard';
 
   private applicationConfigService = inject(ApplicationConfigService);
+  private dpConfig = inject(NgbDatepickerConfig);
 
   constructor() {
     this.applicationConfigService.setEndpointPrefix(SERVER_API_URL);
+    registerLocaleData(locale);
+    this.dpConfig.minDate = { year: dayjs().subtract(100, 'year').year(), month: 1, day: 1 };
   }
 }
