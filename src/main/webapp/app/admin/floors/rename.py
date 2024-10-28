305b5b6120cd622e9c23ae5185f5f4c930208fb9
Arsen A <powerm4f12@gmail.com>
2024-10-28 09:38:02 -0400

    removed rename.py

--- src/main/webapp/app/admin/floors/rename.py
+++
@@ -1,7 +0,0 @@
-import os
-
-for root, dirs, files in os.walk('.'):
-    for f in files:
-        f_new = f.replace("floor", "floor")
-        print(f_new)
-        os.rename(os.path.join(root, f), os.path.join(root, f_new))
